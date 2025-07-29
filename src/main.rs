a7dba0cecce0c0211b14c6d5954538716f62c799
leolegge <llegge1@sheffield.ac.uk>
2025-07-29 13:38:15 +0100

    Learning how to encrypt and decrypt structs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::io;
 use std::fs;
 use std::fs::File;
-use age::secrecy::Secret;
-use age::armor::ArmoredWriter;
-use std::io::{Read, Write};
-use age::Encryptor;
+
+use age::armor::{ArmoredWriter, Format};
+
+use age::{Decryptor, Encryptor};
 use serde::{Serialize, Deserialize};
-use std::io::{BufWriter};
+
+use secrecy::{ExposeSecret, Secret};
+use std::io::{BufReader, Read, Write};
 
 
 
 
-
+#[derive(Serialize, Deserialize, Debug)]
 struct Vault {
     entries : Vec<Entry>,
 }
@@ -26,7 +28,7 @@ impl Vault {
     }
 }
 
-
+#[derive(Serialize, Deserialize, Debug)]
 struct Entry{
     identifier: Vec<u8>,
     password: Vec<u8>,
@@ -75,6 +77,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
                     .expect("Failed to read line");
                 
                 let new_vault = Vault::new();
+
+                let passphrase = Secret::new(
+                    rpassword::prompt_password("Enter passphrase for your new vault: ")
+                        .expect("Failed to read passphrase")
+                );
+                
+                println!("{:?}", passphrase);
+                
+                
+                
+                save_vault_to_file(new_vault, passphrase, &new_name)?;
+                
                 
                 
             }
@@ -94,7 +108,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
     
     
     
-    
+    //EXAMPLE //////////////////////////////////////////////////////////////////////////////////
     
     
     let plaintext = b"Hello world!";
@@ -118,11 +132,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
     let plaintext_str = String::from_utf8(decrypted)?;
     println!("Decrypted text: {}", plaintext_str);
 
+    //////////////////////////////////////////////////////////////////////////////////////////
+
     
     Ok(())
 }
 
-pub fn encrypt_with_passphrase(passphrase: &str, plaintext: &[u8], ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn encrypt_with_passphrase(passphrase: &str, plaintext: &[u8], ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let encryptor = age::Encryptor::with_user_passphrase(Secret::new(passphrase.to_owned()));
 
     let mut encrypted = vec![];
@@ -134,7 +150,7 @@ pub fn encrypt_with_passphrase(passphrase: &str, plaintext: &[u8], ) -> Result<V
     Ok(encrypted)
 }
 
-pub fn decrypt_with_passphrase(passphrase: &str, encrypted: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn decrypt_with_passphrase(passphrase: &str, encrypted: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     
     let decryptor = match age::Decryptor::new(&encrypted[..])? {
             age::Decryptor::Passphrase(d) => d,
@@ -148,7 +164,7 @@ pub fn decrypt_with_passphrase(passphrase: &str, encrypted: &[u8]) -> Result<Vec
     Ok(decrypted)
 }
 
-pub fn get_all_vaults(){
+fn get_all_vaults(){
     //TODO example code for getting all directories in root
     let paths = fs::read_dir("./").unwrap();
 
@@ -157,12 +173,44 @@ pub fn get_all_vaults(){
     }
 }
 
-pub fn save_vault_to_file(vault: Vault ,passphrase: &str ,filename: &str) -> Result<(), Box<dyn std::error::Error>> {
 
-    let vault_path = std::env::current_exe()?.parent().unwrap().to_path_buf().join(filename);
-
+//TODO learn how to write to a file
+fn save_vault_to_file(vault: Vault ,passphrase: Secret<String> ,filename: &str) -> Result<(), Box<dyn std::error::Error>> {
     
+    // 2. Serialize to JSON
+    let json = serde_json::to_string(&vault)?;
+
+    // 3. Open file to write encrypted output
+    let file = File::create([filename, ".age"].concat())?;
+    let armor = ArmoredWriter::wrap_output(file, Format::Binary)?;
+
+    // 4. Create encryptor with passphrase
+    let encryptor = Encryptor::with_user_passphrase(passphrase);
+
+    // 5. Encrypt and write
+    let mut writer = encryptor.wrap_output(armor)?;
+    writer.write_all(json.as_bytes())?;
+    writer.finish()?;
+
+    println!("Encrypted and saved");
 
     Ok(())
 }
 
+
+//TODO learn how to read from a file
+/*
+fn load_vault_from_file(filename: &str, passphrase: Secret<String> ) -> Result<Vault, Box<dyn std::error::Error>> {
+    let vault_path = std::env::current_exe()?.parent().unwrap().to_path_buf().join([filename, ".age"].concat());
+    
+    let file = File::open(vault_path)?;
+    let mut reader = BufReader::new(file);
+
+    
+
+
+    Ok(())
+}
+
+
+ */
